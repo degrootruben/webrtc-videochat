29de0970f33ef555025edaf690447bb059237ac9
Ruben de Groot <rpg.degroot@gmail.com>
2021-03-21 17:40:27 +0100

    Added fix for serving index.html in production

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const http = require("http").createServer(app);
 const port = process.env.PORT || 5000;
 
@@ -12,6 +13,11 @@ if (process.env.NODE_ENV === "development") {
     });
 } else if (process.env.NODE_ENV === "production") {
     io = require("socket.io")(http);
+
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
 const connectedUsers = {};
